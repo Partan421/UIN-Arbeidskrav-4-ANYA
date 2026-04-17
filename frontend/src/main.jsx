92cc674b34dcd7b41b33bea0063d25be1bb2a605
Partan421 <myzahid@hiof.no>
2026-04-17 13:23:36 +0200

    removed duplicate code

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,4 @@
 import { createRoot } from 'react-dom/client'
-import { createRoot } from 'react-dom/client'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom'
 import App from './App.jsx'
