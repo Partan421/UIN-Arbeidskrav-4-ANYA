035b389e86820e2d1f6cb1ed1c5957bc36a393ff
AnnaChe97 <anna_nguyen97@hotmail.com>
2026-04-20 15:33:52 +0200

    Lagt til Sanity struktur
    Lagt til kode for tittel og beskrivelse av arbeidskrav

---
+++ backend/schemaTypes/arbeidskrav.js
@@ -0,0 +1,19 @@
+const arbeidskrav = {
+    name: "arbeidskrav",
+    title: "arbeidskrav",
+    type: "document",
+    fields: [
+        {
+            name: "title",
+            title: "arbeidskrav",
+            type: "string"
+        },
+        {
+            name: "description",
+            title: "Beskrivelse",
+            type: "text"
+        },
+    ]
+}
+
+export default arbeidskrav
