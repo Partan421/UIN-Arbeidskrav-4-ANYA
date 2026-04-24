2b1ec8287c5a08552ff131f6211cff41a2b19e90
AnnaChe97 <anna_nguyen97@hotmail.com>
2026-04-24 19:05:26 +0200

    HTML-struktur
    Fullført html-struktur for profilen

--- frontend/src/components/AnnaProfil.jsx
+++ frontend/src/components/AnnaProfil.jsx
@@ -7,6 +7,27 @@ export default function AnnaProfil() {
             <figure>
                 <img/>
             </figure>
+            <article>
+                <h3>Oversikt</h3>
+                <ul>
+                    <li>
+                        Skole E-mail:
+                        <a href="mailto:aangyen@hiof.no"
+                        className="email-lenke">
+                            aanguyen@hiof.no
+                        </a>
+                    </li>
+                    <li>
+                        Privat E-mail:
+                        <a href="mailto:anna_nguye97@hotmail.com"
+                        className="email-lenke">
+                            anna_nguyen97@hotmail.com
+                        </a> 
+                    </li>
+                    <li>Telefon nummer: 46440617</li>
+                    <li>Studier: Bachelorstudium i informasjonssystemer</li>
+                </ul>
+            </article>
         </section>
     )
 }
