61c8401d2f865e28655a63b4fb5cee47c8a43216
AnnaChe97 <anna_nguyen97@hotmail.com>
2026-04-24 17:41:58 +0200

    Startet på profil
    Begynt med html struktur for profil

---
+++ frontend/src/components/AnnaProfil.jsx
@@ -0,0 +1,12 @@
+export default function AnnaProfil() {
+    return(
+        <section>
+            <header>
+                <h2>Anna</h2>
+            </header>
+            <figure>
+                <img/>
+            </figure>
+        </section>
+    )
+}
