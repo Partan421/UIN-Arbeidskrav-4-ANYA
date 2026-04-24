771238ad401ddc79b0efea31b9a34c8b3db07e31
AnnaChe97 <anna_nguyen97@hotmail.com>
2026-04-24 22:16:18 +0200

    Laget css
    Laget css styling til html

---
+++ frontend/src/components/AnnaProfil.jsx
@@ -0,0 +1,37 @@
+import "../Style/AnnaProfil.scss"
+import bildeForAnna from "../Images/Yuko-xHolic.jpg";
+
+
+export default function AnnaProfil() {
+    return(
+        <section className="Annas-profil">
+            <header>
+                <h2>Anna</h2>
+            </header>
+            <figure>
+                <img src={bildeForAnna} alt="Bildet for Anna"/>
+            </figure>
+            <article>
+                <h3>Oversikt:</h3>
+                <ul>
+                    <li>
+                        Skole E-mail:
+                        <a href="mailto:aangyen@hiof.no"
+                        className="email-lenke">
+                            aanguyen@hiof.no
+                        </a>
+                    </li>
+                    <li>
+                        Privat E-mail:
+                        <a href="mailto:anna_nguye97@hotmail.com"
+                        className="email-lenke">
+                            anna_nguyen97@hotmail.com
+                        </a> 
+                    </li>
+                    <li>Telefon nummer: 46440617</li>
+                    <li>Studier: Bachelorstudium i informasjonssystemer</li>
+                </ul>
+            </article>
+        </section>
+    )
+}
